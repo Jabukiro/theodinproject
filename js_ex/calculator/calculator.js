6ed40c83d6522a0860b6084c55bc7ba35d7f61f3
jabukiro <d.barihuta@gmail.com>
2020-03-17 19:32:11 +0800

    Passed all tests succesfully

--- js_ex/calculator/calculator.js
+++ js_ex/calculator/calculator.js
@@ -1,25 +1,37 @@
-function add () {
-	
+function add (int1, int2) {
+	return int1+int2;
 }
 
-function subtract () {
-	
+function subtract (int1, int2) {
+	return int1-int2;
 }
 
-function sum () {
-	
+function sum (intArr) {
+	var total = 0;
+	for(i=0;i<intArr.length;i++){
+		total+=intArr[i];
+	}
+	return total;
 }
 
-function multiply () {
-	
+function multiply (intArr) {
+	var total = 1;
+	for(i=0;i<intArr.length;i++){
+		total*=intArr[i];
+	}
+	return total;	
 }
 
-function power() {
-	
+function power(int1, int2) {
+	return Math.pow(int1, int2)
 }
 
-function factorial() {
-	
+function factorial(int1) {
+	var total = 1;
+	for(i=int1;i>0;i--){
+		total*=i;
+	}
+	return total;
 }
 
 module.exports = {
